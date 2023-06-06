e535e71096e7939611ec869d85406a1a96044105
Mubashir Saeed <mubashir5085@gmail.com>
2023-06-06 17:32:23 +0500

    Created Project CUBOID Chat Bot

---
+++ android/app/src/main/kotlin/com/example/cuboid_chat_bot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cuboid_chat_bot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cuboid_chat_bot/views/chat_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ChatPage(),
+  ));
+}
+
