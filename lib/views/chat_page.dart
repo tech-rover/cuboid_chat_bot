181b10d23aa72a0bfeed909c5c7126996402fe7e
Mubashir Saeed <mubashir5085@gmail.com>
2023-06-06 17:56:43 +0500

    UI Ready

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -5,8 +5,24 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true,title: Text("CUBOID Chat Bot")),
-      body: Column(
-        
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: ListView(
+              padding: EdgeInsets.only(bottom: 60),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: "Start your chat"),
+              ),
+            ))
+        ],
       ),
     );
   }
